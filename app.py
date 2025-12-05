9fbd675876acd0d547b14d84b85b56287a57e681
Filipe <fspereiramanagement@gmail.com>
2025-12-05 21:29:19 +0100

    video change

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
 import streamlit as st
 import tempfile
-from model import model
-import os
+from model import predict_video, model
 
 
 st.set_page_config(page_title="VisAI: Object Detection", layout="wide")
 st.title("Drone Vision: Object Detection")
 
+st.markdown(
+    """
+    <style>
+    section[data-testid="stSidebar"] {
+        width: 350px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True,
+)
+
 with st.sidebar:
     video_file = st.file_uploader("Upload Video", type=["mp4", "mov", "avi"])
-    confidence_threshold = st.slider("Confidence Threshold", 0.0, 1.0, 0.80, 0.05)
 
 if video_file:
-    temp_input = tempfile.NamedTemporaryFile(delete=False, suffix='.mp4')
-    temp_input.write(video_file.read())
-    video_path = temp_input.name
-    temp_input.close()
+    temp = tempfile.NamedTemporaryFile(delete=False, suffix='.mp4')
+    temp.write(video_file.read())
+    video_path = temp.name
+    temp.close()
 
-    if st.button("Run Detection"):
-        with st.spinner("Processing video..."):
-            
-            results = model.predict(
-                source=video_path,
-                save=True,
-                conf=confidence_threshold,
-                project='runs/detect',
-                name='output',
-                exist_ok=True
-            )
-            
-            
-            output_path = 'runs/detect/output/video_path.avi' 
-            
-            st.success("Processing complete!")
-            st.video(output_path)
+    if st.button("Run Prediction"):
+        st.write("Processing video, please wait...")
+        with st.sidebar:
+            st.write("![Your Awesome GIF](https://i.makeagif.com/media/10-08-2020/QMj7da.gif)")
+
+        frame_placeholder = st.empty()
+        
+        # Call predict_video with frame placeholder
+        predict_video(video_path, frame_placeholder)
+        
+        st.success("Processing complete!")
